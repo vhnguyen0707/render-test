867f8e71859507851839810dd3bdee151cc83be9
vhnguyen0707 <nhvu@ualberta.ca>
2023-04-06 21:51:33 -0700

    Added custom validation for person model's number

--- models/Person.js
+++ models/Person.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose')
 
 const personSchema = new mongoose.Schema({
     name: { type: String, minLength: 3, required: true },
-    number: String
+    number: { type: String, 
+            validate: {
+                validator: function(v){
+                    return /\d{2,3}-\d/.test(v);
+                },
+                message: props => `${props.value} is not a valid phone number!`
+            },
+            required: [true, 'User phone number required']}
 })
 
 personSchema.set('toJSON', {
